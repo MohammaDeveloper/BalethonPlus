bcb3d6fdbffc664c1933cfac97d18f1596f712b4
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-06-02 08:11:38 +0330

    feat: Refactor `ChatAction` enum
    This commit refactors the `ChatAction` enum by:
    
    - Replacing `UPLOAD_DOCUMENT` and `UPLOAD_VOICE` with `UPLOAD_AUDIO`.
    - Changing the type hint for `ChatAction` in `Client._prepare_request_data` to `NameEnum` to allow for more generic enum handling.
    
    These changes simplify the `ChatAction` enum and improve the flexibility of the `Client` class.

--- balethon/enums/chat_action.py
+++ balethon/enums/chat_action.py
@@ -5,8 +5,7 @@ from .name_enum import NameEnum
 
 class ChatAction(NameEnum):
     TYPING = auto()
-    UPLOAD_DOCUMENT = auto()
+    UPLOAD_AUDIO = auto()
     UPLOAD_PHOTO = auto()
-    UPLOAD_VOICE = auto()
     UPLOAD_VIDEO = auto()
     RECORD_VOICE = auto()

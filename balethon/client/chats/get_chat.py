e50b7c222b5948b98f5e054e2154711cdef43f57
MohammaDeveloper <vatankhahmohammad19384@gmail.com>
2025-06-09 13:32:13 +0330

    feat: Refactor `get_chat` method and update `Chat` object properties
    This commit refactors the `get_chat` method in `balethon/client/chats/get_chat.py` to improve code structure and clarity. It also updates the `Chat` object in `balethon/objects/chat.py` to handle different chat types more effectively.
    
    - Modified the `get_chat` method to construct and return a `Chat` object based on the API response, improving code readability.
    - Updated the `get_chat` method to handle different chat types (private, group, channel) and set the corresponding properties of the `Chat` object.
    - Modified the `Chat` object to directly assign the `type` property without re-enum lookup.

--- balethon/client/chats/get_chat.py
+++ balethon/client/chats/get_chat.py
@@ -8,11 +8,34 @@ from ...enums import ChatType
 
 class GetChat:
     async def get_chat(self: "balethon.Client", chat_id: Union[int, str]) -> Chat:
+        result = Chat()
+
         # 1234567890 | "1234567890"
         if isinstance(chat_id, int) or (
             isinstance(chat_id, str) and chat_id.isnumeric()
         ):
-            return Chat(await self.auto_execute("get", "getChat", locals()))
+            info = await self.auto_execute("get", "getChat", locals())
+
+            result.id = info.id
+            result.username = info.username
+            result.description = info.description
+
+            if info.type == "private":
+                result.type = ChatType.PRIVATE
+                result.first_name = info.first_name     
+                result.bind(self)
+                return result
+
+            result.title = info.title
+
+            if info.type == "group":
+                result.type = ChatType.GROUP
+
+            elif info.type == "channel":
+                result.type = ChatType.CHANNEL
+
+            result.bind(self)    
+            return result
 
         # "@username"
         if chat_id.startswith("@"):
@@ -29,8 +52,6 @@ class GetChat:
         # "ble.ir/join/ABCDEEFGHI" | "ble.ir/username"
         elif chat_id.startswith("ble.ir/"):
             chat_id = chat_id.replace("ble.ir/", "")
-
-        result = Chat()
 
         info = await self.connection.get_peer_info(chat_id)
 
